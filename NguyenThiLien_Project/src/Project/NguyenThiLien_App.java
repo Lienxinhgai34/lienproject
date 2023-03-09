c2bc11b3339839bacf837bd93fdf85d4b63b1761
NguyenLien <97594993+Lienxinhgai34@users.noreply.github.com>
2023-03-09 19:42:45 +0700

    baitap

---
+++ NguyenThiLien_Project/src/Project/NguyenThiLien_App.java
@@ -0,0 +1,9 @@
+public class NguyenThiLien_App {
+    public static void main(String[] args) {
+     System.out.println("Nguyen Thi Lien Hoc Java");
+     //tesst
+     
+     System.out.print("2021050383");
+    }
+    
+ }
